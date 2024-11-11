6f09b9028e94ab90b9325382c5369e7424621268
jgdisenio <j.gazzin@gmail.com>
2024-11-10 21:24:49 -0300

    inicial

---
+++ assets/js/arquitectura.js
@@ -0,0 +1,84 @@
+export const arq = [
+    {
+        'nombre': 'bienvenido',
+        'options': [
+            'ingresar',
+            'registrarse',
+        ],
+    },
+    {
+        'nombre': 'panel',
+        'options': [
+            'perfil',
+            'proyectos',
+        ]
+    },
+    {
+        'nombre': 'proyectos',
+        'options': [
+            `nombre`,
+            'monto',
+            'avance',
+            'activa'
+        ]
+    },
+    {
+        'nombre': 'obra',
+        'options': [
+            'información',
+            'computo',
+            'presupuesto',
+            'avance',
+            'materiales',
+            'servicios',
+            'certificados',
+            'notas'
+        ]
+    },
+    {
+        'nombre': 'items',
+        'options': {
+            'información': [
+                'propietario',
+                'ubicación',
+                'dirección',
+                'constructor'
+            ],
+            'rubros': [
+                'preliminares',
+                'fundaciones',
+                'cerramientos',
+                'cubiertas',
+                'aberturas',
+                'terminaciones',
+                'instalaciones',
+                'otros'
+            ]
+        }
+    },
+    {
+        'nombre': 'detalle',
+        'options': {
+            'perfil': [
+                'nombre',
+                'apellido',
+                'email',
+                'telefono',
+                'ocupación'
+            ],
+            'ubicacion': [
+                'calle',
+                'numero',
+                'piso',
+                'ciudad',
+                'provincia',
+            ],
+            'tareas': [
+                'nombre',
+                'valor',
+                'unidad'    
+            ]
+        }
+    }
+
+]
