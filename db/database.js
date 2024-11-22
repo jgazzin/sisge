ebe31c12a706fddd1eb6436948d76566fb22348d
jgdisenio <j.gazzin@gmail.com>
2024-11-22 14:05:11 -0300

    corrección en database

--- db/database.js
+++ db/database.js
@@ -28,41 +28,41 @@ connection.connect((err) => {
         return;
       }
       console.log("usuario cambiado");
-    });
 
-    // tabla ususarios
-    const createtableUsuariosQuery = `
-    CREATE TABLE IF NOT EXISTS sisge_usuarios(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(50) NOT NULL,
-    password VARCHAR(10) NOT NULL,
-    perfil INT,
-    contacto INT,
-    direccion INT);`;
+      // tabla ususarios
+      const createtableUsuariosQuery = `
+        CREATE TABLE IF NOT EXISTS sisge_usuarios(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        email VARCHAR(50) NOT NULL,
+        password VARCHAR(10) NOT NULL,
+        perfil INT,
+        contacto INT,
+        direccion INT);`;
 
-    connection.query(createtableUsuariosQuery, (err, result) => {
-      if (err) {
-        console.log("Error creando la tabla ususarios: ", err);
-        return;
-      }
-      console.log("Tabla usuarios asegurada");
-    });
+      connection.query(createtableUsuariosQuery, (err, result) => {
+        if (err) {
+          console.log("Error creando la tabla ususarios: ", err);
+          return;
+        }
+        console.log("Tabla usuarios asegurada");
+      });
 
-    // tabla perfiles
-    const createtablePerfilesQuery = `
-    CREATE TABLE IF NOT EXISTS sisge_perfiles(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL,
-    apellido VARCHAR(50),
-    oficio VARCHAR(50),
-    id_user INT NOT NULL);`;
+      // tabla perfiles
+      const createtablePerfilesQuery = `
+        CREATE TABLE IF NOT EXISTS sisge_perfiles(
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nombre VARCHAR(50) NOT NULL,
+        apellido VARCHAR(50),
+        oficio VARCHAR(50),
+        id_user INT NOT NULL);`;
 
-    connection.query(createtablePerfilesQuery, (err, result) => {
-      if (err) {
-        console.log("Error creando la tabla perfiles: ", err);
-        return;
-      }
-      console.log("Tabla perfiles asegurada");
+      connection.query(createtablePerfilesQuery, (err, result) => {
+        if (err) {
+          console.log("Error creando la tabla perfiles: ", err);
+          return;
+        }
+        console.log("Tabla perfiles asegurada");
+      });
     });
   });
 });
