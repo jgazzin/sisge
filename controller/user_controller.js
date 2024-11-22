28813bd562d040ef110e6512e30017864de51add
jgdisenio <j.gazzin@gmail.com>
2024-11-22 11:46:20 -0300

    db freesql

--- controller/user_controller.js
+++ controller/user_controller.js
@@ -1,7 +1,7 @@
 const db = require('../db/database');
 
 const obtenerUsuarios = (req, res) => {
-    const sql = 'SELECT * FROM usuarios';
+    const sql = 'SELECT * FROM sisge_usuarios';
     db.query(sql, (err, result) => {
         if(err){
             console.log('Error obteniendo usuarios: ', err);
@@ -14,7 +14,7 @@ const obtenerUsuarios = (req, res) => {
 
 const crearUsuario = (req, res) => {
     const {email, password} = req.body;
-    const sql = 'INSERT INTO usuarios (email, password) VALUES (?, ?)';
+    const sql = 'INSERT INTO sisge_usuarios (email, password) VALUES (?, ?)';
 
     db.query(sql, [email, password], (err, result) => {
         if(err){
@@ -28,7 +28,7 @@ const crearUsuario = (req, res) => {
 
 const obtenerUserID = (req, res) => {
     const {id} = req.params;
-    const sql = 'SELECT * FROM usuarios WHERE id = ?';
+    const sql = 'SELECT * FROM sisge_usuarios WHERE id = ?';
 
     db.query(sql, [id], (err, result) => {
         if(err) {
