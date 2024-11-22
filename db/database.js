0921ce48b6d83d2c84d2886dcde1f9579a7da839
jgdisenio <j.gazzin@gmail.com>
2024-11-22 10:24:31 -0300

    enviroment

--- db/database.js
+++ db/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     //database: 'sisge_bd'
 });
 
